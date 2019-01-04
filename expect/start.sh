49bf00f92e72ff7e62c27d5d20217efd3f97a604
meredithzj <13182828958@163.com>
2019-01-04 20:09:02 +0800

    3 commit

---
+++ expect/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+n=`cat 1.txt|wc -l`
+for i in `seq 1 $n`
+do
+user_1=`sed -n "$i"p /root/jiaoben1/expect/1.txt |awk -F":" '{print $1}'`
+ip_1=`sed -n "$i"p  /root/jiaoben1/expect/1.txt |awk -F":" '{print $2}'`
+passwd_1=`sed -n "$i"p /root/jiaoben1/expect/1.txt|awk -F":" '{print $3}'`
+./3.exp $user_1 $ip_1 $passwd_1
+done
